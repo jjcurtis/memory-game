d9a46413ac3b175c79a7cdc7f675c9f9857eed84
Jonathan Curtis <jonathan.j.curtis@outlook.com>
2023-05-31 20:06:25 -0500

    Create Header with basic score state

---
+++ app/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+import style from '../style/Header.module.css'
+
+type Props = {
+  score: number;
+  highScore: number;
+}
+
+export function Header({ score, highScore }: Props) {
+  return (
+    <header className={style.header}>
+        <Image className={style.logo} src='/header.svg' alt='Pokemon Memory game header' width={400} height={100}/>
+        <div className={style.scoreboard} role='scoreboard'>
+          <span>High Score: {highScore}</span>
+          <span>Score: {score}</span>
+        </div>
+
+    </header>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import styles from './page.module.css'
+"use client"
+import { useState } from 'react'
+import { Header } from './components/Header'
+import style from './page.module.css'
 
 export default function Home() {
+  const [score, setScore] = useState(0);
+
   return (
     <main>
-
+      <Header score={score} highScore={score}/>
     </main>
   )
 }
